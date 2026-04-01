4b837c55b32b7ad7c398c4887e61df33819ec1c0
bikashrajkhowa-i12 <bikashrajkhowa424265@gmail.com>
2026-04-01 23:20:56 +0530

    feat: :sparkles: JS-built-in trim mechanics

---
+++ 01-basics/built-in/trim-mechanics.js
@@ -0,0 +1,6 @@
+const input = "   hello world   ";
+console.log("input string:", input);
+
+console.log("trim:", input.trim()); // "hello world"
+console.log("trimStart:", input.trimStart()); // "hello world   "
+console.log("trimEnd:", input.trimEnd()); // "   hello world"
